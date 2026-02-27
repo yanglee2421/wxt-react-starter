07b6da22b7a1e183b5437b0b84e48a8315f910cd
yanglee2421 <yanglee2421@gmail.com>
2026-02-27 14:56:47 +0800

    wip[serve]: add session table

--- packages/serve/src/db/index.ts
+++ packages/serve/src/db/index.ts
@@ -4,10 +4,11 @@ import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import * as schema from "./schema";
+import { relations } from "./relations";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const sqliteDatabase = new Database(path.resolve(__dirname, "./data.db"));
-export const db = drizzle({ schema, client: sqliteDatabase });
+export const db = drizzle({ schema, client: sqliteDatabase, relations });
 
 export const runMigrations = () => {
   migrate(db, {
--- packages/serve/src/db/schema.ts
+++ packages/serve/src/db/schema.ts
@@ -1,12 +1,23 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export type User = typeof users.$inferSelect;
+export type Session = typeof sessions.$inferSelect;
 
 export const users = sqliteTable("users", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name"),
   email: text("email"),
   password: text("password"),
+  createdAt: integer("createdAt", { mode: "timestamp" }),
+  updatedAt: integer("updatedAt", { mode: "timestamp" }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const sessions = sqliteTable("sessions", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  userId: integer("userId").references(() => users.id),
+  token: text("token"),
   createdAt: integer("createdAt", { mode: "timestamp" }),
   updatedAt: integer("updatedAt", { mode: "timestamp" }).$onUpdate(
     () => new Date(),
